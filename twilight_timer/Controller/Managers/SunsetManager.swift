e04082cb1ea5d3605f6b365b6aef7165c1735405
justinmajetich <justinmajetich@gmail.com>
2021-08-05 13:54:44 -0400

    Rework sunset update logic to always provide next sunset time

--- twilight_timer/Controller/Managers/SunsetManager.swift
+++ twilight_timer/Controller/Managers/SunsetManager.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2020 Justin Majetich. All rights reserved.
 //
 
-import UIKit
 import Foundation
 
 class SunsetManager {
@@ -14,21 +13,40 @@ class SunsetManager {
     let locationManager = LocationManager()
     let storage = StorageManager()
     
-    var delegate: SunsetManagerDelegate?
-    var currentSunset: SunsetModel?
-
-    init() {
-        // Initialize currentSunset from last stored model.
-        // currentSunset will be nil if load fails
-        loadSunsetFromDisk()
-        // Initialize observer to monitor notification of location update
-        addLocationUpdateObserver()
-    }
+    var sunset: SunsetModel?
     
-    // Method triggers location update. Delegate method
-    // didUpdateSunset will be called indirectly on success
-    func updateSunset() {
-        self.locationManager.requestLocationUpdate()
+    init() {
+       
+        loadSunsetFromDisk()
+        
+        // Initialize observer to monitor notification of location update
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(didUpdateLocations),
+            name: K.didUpdateLocation,
+            object: locationManager
+        )
+                
+        locationManager.requestLocationUpdate()
+    }
+        
+    // This function is called whenever the sunset model is updated in order to handle follow up behavior
+    private func didUpdateSunset() {
+        
+        // Once today's twilight has ended, update sunset to reflect next sunset.
+        DispatchQueue.main.asyncAfter(deadline: .now() + (sunset!.nextSunset.timeIntervalSinceNow + K.twilightDuration)) {
+            
+            self.fetchSunsetData(
+                lat: self.locationManager.currentLatitude ?? "",
+                lon: self.locationManager.currentLongitude ?? ""
+            )
+        }
+        
+        NotificationCenter.default.post(
+            name: K.didUpdateSunset,
+            object: self,
+            userInfo: ["nextSunset": sunset!.nextSunset]
+        )
     }
 }
 
@@ -40,22 +58,21 @@ private extension SunsetManager {
         
         let apiKey = K.openWeatherAPIKey
         let apiURL = K.openWeatherAPIURL
+        let exclude = K.openWeatherApiRequestExclusions
        
-        if let url = URL(string: "\(apiURL)?lat=\(lat)&lon=\(lon)&appid=\(apiKey)") {
+        if let url = URL(string: "\(apiURL)?lat=\(lat)&lon=\(lon)&exclude=\(exclude)&appid=\(apiKey)") {
             
             let session = URLSession(configuration: .default)
             
             // Define session task with completetion handler
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    self.delegate?.didFailUpdateWithError(error: error!)
                     return
                 } else {
                     if let safeData = data {
                         // Parse JSON data package
                         if let sunsetData = self.parseJSON(data: safeData) {
-                            self.updateCurrentSunsetWithData(in: sunsetData)
-                            self.delegate?.didUpdateSunset(manager: self, self.currentSunset!)
+                            self.updateSunsetWithData(in: sunsetData)
                             self.saveSunsetToDisk()
                         }
                     }
@@ -64,6 +81,23 @@ private extension SunsetManager {
             // Start session task
             task.resume()
         }
+    }
+    
+    private func updateSunsetWithData(in sunsetData: SunsetData) {
+        
+        // If now is before the end of today's twilight, use today's twilight for current model
+        if Date() < sunsetData.daily[0].sunset.addingTimeInterval(K.twilightDuration) {
+            sunset = SunsetModel(
+                nextSunset: sunsetData.daily[0].sunset
+            )
+        // Otherwise, assign following sunset to current.
+        } else {
+            sunset = SunsetModel(
+                nextSunset: sunsetData.daily[1].sunset
+            )
+        }
+        
+        didUpdateSunset()
     }
     
     // Decode JSON response from the OpenWeatherMap API
@@ -76,53 +110,24 @@ private extension SunsetManager {
             let weatherData = try decoder.decode(SunsetData.self, from: data)
             return weatherData
         } catch {
-            self.delegate?.didFailUpdateWithError(error: error)
             return nil
         }
     }
-    
-    func updateCurrentSunsetWithData(in data: SunsetData) {
-        // If model is already initialized, update with fetched data.
-        // If not, initialize new model with data.
-        if currentSunset != nil {
-            currentSunset?.sunsetTime = data.sys.sunset
-            currentSunset?.latitude = data.coord.lat
-            currentSunset?.longitude = data.coord.lon
-            currentSunset?.placeName = data.name
-        } else {
-            currentSunset = SunsetModel(sunsetTime: data.sys.sunset,
-                                        latitude: data.coord.lat,
-                                        longitude: data.coord.lon,
-                                        placeName: data.name)
-        }
-    }
-    
-    
 }
 
 //MARK: - Notification Observance
 
 private extension SunsetManager {
-    
-    // Creates notification observer to monitor location updates
-    func addLocationUpdateObserver() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(didUpdateLocations),
-                                               name: K.didUpdateLocation,
-                                               object: locationManager)
-    }
-    
-    // Triggers when CLLocationManagerDelegate method
-    // didUpdateLocations is called and location manager
-    // has successfully updated coordinates
+       
+    // Triggers when CLLocationManagerDelegate method didUpdateLocations is called
+    // and location manager has successfully updated coordinates
     @objc func didUpdateLocations(_ notification: Notification) {
-    
+            
         if let updatedCoordinates = notification.userInfo as? [String: String] {
             fetchSunsetData(lat: updatedCoordinates["latitude"]!,
                             lon: updatedCoordinates["longitude"]!)
         }
     }
-    
 }
 
 
@@ -133,13 +138,15 @@ extension SunsetManager {
     func loadSunsetFromDisk() {
         // load() will return lastest sunset model, or nil on failure
         if let loadedSunset: SunsetModel = storage.load(from: K.sunsetStorageFilename) {
-            currentSunset = loadedSunset
-            self.delegate?.didUpdateSunset(manager: self, self.currentSunset!)
+            
+            sunset = loadedSunset
+            
+            didUpdateSunset()
         }
     }
     
     func saveSunsetToDisk() {
-        storage.save(currentSunset, to: K.sunsetStorageFilename)
+        storage.save(sunset, to: K.sunsetStorageFilename)
     }
 
 }
